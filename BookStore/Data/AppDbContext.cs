ef9da81e6b4445ad53140f4e5148d0d78fce6d22
Julien Ospital <ospital.julien@gmail.com>
2024-01-09 10:45:33 +0100

    Creating local database and connect it to app

---
+++ BookStore/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
--- BookStore/Models/Category.cs
+++ BookStore/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace BookStore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
