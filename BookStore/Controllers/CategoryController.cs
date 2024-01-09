65f6c4a348b7a67fc746a5d226c51d53e5346273
Julien Ospital <ospital.julien@gmail.com>
2024-01-09 11:39:03 +0100

    Add Category + CSS

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+        public CategoryController(AppDbContext db)
+        {
+            _db = db; 
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+    }
+}
+
--- BookStore/Data/AppDbContext.cs
+++ BookStore/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Data
 {
@@ -7,6 +8,17 @@ namespace BookStore.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
                 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
         }
     }
 }
