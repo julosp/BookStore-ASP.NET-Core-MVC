9f78b2c8b910bec0fd302f147b990684e752f019
Julien Ospital <ospital.julien@gmail.com>
2024-01-19 11:38:32 +0100

    Add product to database

---
+++ BookStore.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using BookStore.DataAccess.Data;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private AppDbContext _db;
+        public ProductRepository(AppDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
--- BookStore.DataAccess/Repository/UnitOfWork.cs
+++ BookStore.DataAccess/Repository/UnitOfWork.cs
@@ -12,10 +12,14 @@ namespace BookStore.DataAccess.Repository
     {
         private AppDbContext _db;
         public ICategoryRepository Category { get; private set; }
+
+        public IProductRepository Product {  get; private set; }
+
         public UnitOfWork(AppDbContext db)
         { 
-         _db = db;
+            _db = db;
             Category = new CategoryRepository(_db);
+            Product = new ProductRepository(_db);
         }   
 
         
